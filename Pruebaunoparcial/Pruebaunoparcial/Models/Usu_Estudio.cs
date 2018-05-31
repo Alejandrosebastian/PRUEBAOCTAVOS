7369d8a8952a5890ae5d7d99cc35a264fe1736c8
elmermauricio <39804892+elmermauricio@users.noreply.github.com>
2018-05-31 14:28:38 -0500

    Actual

---
+++ Pruebaunoparcial/Pruebaunoparcial/Models/Usu_Estudio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pruebaunoparcial.Models
+{
+    public class Usu_Estudio
+    {
+        public int UsuarioId { get; set; }
+        public int EstudioId { get; set; }
+    }
+}
