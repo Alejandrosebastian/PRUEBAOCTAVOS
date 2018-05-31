0dc00f010f059791e42ec7508beb574bea149a28
Sebastian Alejandro <kanfyslol@gmail.com>
2018-05-31 17:57:46 -0500

    ok no mas

---
+++ Pruebaunoparcial/Pruebaunoparcial/wwwroot/js/Empleo.js
@@ -0,0 +1,82 @@
+﻿
+
+class ClaseEmpleo
+{
+    constructor(empleo, accion)
+    {
+        this.empleo = empleo;
+        this.accion = accion;
+    }
+    Guardarempleo(id) {
+        var empleo = this.empleo;
+        var accion = this.accion;
+        if (id == '0') {
+            $.ajax({
+                type: "POST",
+                url: accion,
+                data: { empleo },
+                success: (respuesta) => {
+                    if (respuesta[0].code == 'save') {
+
+                    }
+                }
+            });
+
+        } else {
+            $.ajax({
+                type: "POST",
+                url: accion,
+                data: { id, empleo },
+                success: (respuesta) => {
+                    if (respuesta[0].code == 'save') {
+
+                    }
+                }
+            });
+        }
+    }
+
+    listadeempleo() {
+        var accion = this.accion;
+        $.ajax({
+            type: "POST",
+            url: accion,
+            data: {},
+            success: (respuesta) => {
+                $.each(respuesta, (index, val) => {
+                    $('#listaempleo').html(val[0]);
+
+                });
+            }
+        });
+
+    }
+
+    EditarEmpleo(empleoId) {
+        var accion = this.accion;
+        $.post(
+            accion,
+            { empleoId },
+            (respuesta) => {
+                document.getElementById('empleo').value = respuesta[0].empleo;
+                document.getElementById('empleoId').value = respuesta[0].empleoId;
+
+            }
+        );
+    }
+
+    EliminaEmpleo(empleoId) {
+        var accion = this.accion;
+        $.ajax(
+            {
+                type: "POST",
+                url: accion,
+                data: { id },
+                success: (respuesta) => {
+                    if (respuesta[0].code == 'Save') {
+
+                    }
+                }
+            });
+    }
+}
