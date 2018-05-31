3e88afcd82a39c940366f075f9f80db010fa16f6
Gabriel <gap5991@hotmail.com>
2018-05-31 17:00:25 -0500

    usuarioModel

--- Pruebaunoparcial/Pruebaunoparcial/wwwroot/js/Usuario.js
+++ Pruebaunoparcial/Pruebaunoparcial/wwwroot/js/Usuario.js
@@ -1,6 +1,27 @@
 ﻿
 class ClaseJSusuario {
-    constructor(nombre, apellido, fecha_nacimiento, sexo, nacionalidad, fecha_alta, direccion, email, telefono, estado_civil, numero_hijos, numero_seguridad_social, identificacion, n_identificacion, permiso_trabajo, permiso_recidencia, empadronado, tipo_licencia) {
+    constructor(nombre, apellido, fecha_nacimiento, sexo, nacionalidad, fecha_alta, direccion, email, telefono, estado_civil, numero_hijos,
+        numero_seguridad_social,tipo, porcentaje, identificacion, n_identificacion, permiso_trabajo, permiso_recidencia, empadronado, tipo_licencia) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.sexo = sexo;
+        this.nacionalidad = nacionalidad;
+        this.fecha_alta = fecha_alta;
+        this.direccion = direccion;
+        this.email = email;
+        this.telefono = telefono;
+        this.estado_civil = estado_civil |;
+        this.numero_hijos = numero_hijos;
+        this.numero_seguridad_social = numero_seguridad_social;
+        this.tipo = tipo;
+        this.porcentaje = porcentaje;
+        this.identificacion = identificacion;
+        this.n_identificacion = n_identificacion;
+        this.permiso_trabajo = permiso_trabajo;
+        this.permiso_recidencia = permiso_recidencia;
+        this.empadronado = empadronado;
+        this.tipo_licencia = tipo_licencia;
 
     }
 
