25e59dfbf653888ece445b0c9125cfccbc6fb395
milto <milto@DESKTOP-LEH3E8Q>
2018-05-31 14:06:57 -0500

    contrato

---
+++ Pruebaunoparcial/Pruebaunoparcial/Models/Contrato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pruebaunoparcial.Models
+{
+    public class Contrato
+    {
+        public int CrontatoId { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [Display(Name = "Escriba el tipo de contrato")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "tenia que poner de 3 a 100 caraccteres")]
+        public int Tipo_contrato { get; set; }
+    }
+}
