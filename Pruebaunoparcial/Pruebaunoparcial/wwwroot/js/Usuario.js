199167fff0225373257ad664a030b3b537cca546
Gabriel <gap5991@hotmail.com>
2018-05-31 18:57:10 -0500

    ya casi

--- Pruebaunoparcial/Pruebaunoparcial/wwwroot/js/Usuario.js
+++ Pruebaunoparcial/Pruebaunoparcial/wwwroot/js/Usuario.js
@@ -54,16 +54,149 @@ class ClaseJSusuario {
                 {
                     type: "POST",
                     url: accion,
-                    data{},
+                    data:
+                    {
+                        nombre, apellido, fecha_nacimiento, sexo, nacionalidad,
+                        fecha_alta, direccion, email, telefono, estado_civil, numero_hijos,
+                        numero_seguridad_social, tipo, porcentaje, identificacion, n_identificacion,
+                        permiso_trabajo, permiso_recidencia, empadronado, tipo_licencia
+                    },
                     success: (respuesta) =>
                     {
                         if (respuesta[0].code == 'Save') {
                             this.limpiarcajas();
                         }
                     }
-                }
-            );
+                });
         }
+        else {
+               $.ajax(
+                {
+                    type: "POST",
+                    url: accion,
+                       data: {
+                           idUsu, nombre, apellido, fecha_nacimiento, sexo, nacionalidad,
+                           fecha_alta, direccion, email, telefono, estado_civil, numero_hijos,
+                           numero_seguridad_social, tipo, porcentaje, identificacion, n_identificacion,
+                           permiso_trabajo, permiso_recidencia, empadronado, tipo_licencia
+                       },
+                    success: (respuesta) => {
+                        if (respuesta[0].code == 'Save') {
+                            this.limpiarcajas();
+                        }
+                    }
+                });
+        }
+
+
+    
+    }
+
+    ListadeUsuario() {
+        var accion = this.accion;
+        $.ajax({
+            type: "POST",
+            url: accion,
+            data: {},
+            success: (respuesta) => {
+                $.each(respuesta, (index, val) => {
+                    $('#ListaUsuario').html(val[0]);
+                });
+            }
+        });
+
+    }
+
+    cargarUsuario(usuarioid) {
+        var accion = this.accion;
+        $.post(
+            accion,
+            { usuarioid },
+            (respuesta) => {
+                document.getElementById('nombre').value = respuesta[0].nombre;
+                document.getElementById('apellido').value = respuesta[0].apellido;
+                document.getElementById('fecha_nacimiento').value = respuesta[0].fecha_nacimiento;
+                document.getElementById('sexo').value = respuesta[0].sexo;
+                document.getElementById('nacionalidad').value = respuesta[0].nacionalidad;
+                document.getElementById('fecha_alta').value = respuesta[0].fecha_alta;
+                document.getElementById('direccion').value = respuesta[0].direccion;
+                document.getElementById('email').value = respuesta[0].email;
+                document.getElementById('telefono').value = respuesta[0].telefono;
+                document.getElementById('estado_civil').value = respuesta[0].estado_civil;
+                document.getElementById('numero_hijos').value = respuesta[0].numero_hijos;
+                document.getElementById('numero_seguridad_social').value = respuesta[0].numero_seguridad_social;
+                document.getElementById('tipo').value = respuesta[0].tipo;
+                document.getElementById('porcentaje').value = respuesta[0].porcentaje;
+        document.getElementById('identificacion').value = respuesta[0].identificacion;
+        document.getElementById('n_identificacion').value = respuesta[0].n_identificacion;
+        document.getElementById('permiso_trabajo').value = respuesta[0].permiso_trabajo;
+        document.getElementById('permiso_recidencia').value = respuesta[0].permiso_recidencia;
+        document.getElementById('empadronado').value = respuesta[0].empadronado;
+        document.getElementById('tipo_licencia').value = respuesta[0].tipo_licencia;
+                               document.getElementById('sexoId').value = respuesta[0].sexoId;
+            }
+
+        )
+    }
+    eliminarUsu(usuid) {
+        var accion = this.accion;
+        $.ajax(
+            {
+                type: "POST",
+                url: accion,
+                data: { usuid },
+                success: (respuesta) => {
+                    if (respuesta[0].code == 'Save') {
+                        this.limpiarcajas();
+                    }
+                }
+            });
+    }
+    limpiarcajas() {
+        document.getElementById('nombre').value = '';
+        document.getElementById('apellido').value = '';
+        document.getElementById('fecha_nacimiento').value ='';
+        document.getElementById('sexo').value = '';
+        document.getElementById('nacionalidad').value = '';
+        document.getElementById('fecha_alta').value = '';
+        document.getElementById('direccion').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('telefono').value = '';
+        document.getElementById('estado_civil').value = '';
+        document.getElementById('numero_hijos').value = '';
+        document.getElementById('numero_seguridad_social').value = '';
+        document.getElementById('tipo').value = '';
+        document.getElementById('porcentaje').value = '';
+        document.getElementById('identificacion').value ='';
+        document.getElementById('n_identificacion').value = '';
+        document.getElementById('permiso_trabajo').value = '';
+        document.getElementById('permiso_recidencia').value = '';
+        document.getElementById('empadronado').value = '';
+        document.getElementById('tipo_licencia').value = '';
+        document.getElementById('sexoId').value = '';
+        
+        $('#nombre').value = '';
+        $('#apellido').value = '';
+        $('#fecha_nacimiento').value = '';
+        $('#sexo').value = '';
+        $('#nacionalidad').value = '';
+        $('#fecha_alta').value = '';
+        $('#direccion').value = '';
+        $('#email').value = '';
+        $('#telefono').value = '';
+        $('#estado_civil').value = '';
+        $('#numero_hijos').value = '';
+        $('#numero_seguridad_social').value = '';
+        $('#tipo').value = '';
+        $('#porcentaje').value = '';
+        $('#identificacion').value = '';
+        $('#n_identificacion').value = '';
+        $('#permiso_trabajo').value = '';
+        $('#permiso_recidencia').value = '';
+        $('#empadronado').value = '';
+        $('#tipo_licencia').value = '';      
+        $('#IngresoUsuario').modal('hide');
+        ListaUsuarios();
     }
 
 }
