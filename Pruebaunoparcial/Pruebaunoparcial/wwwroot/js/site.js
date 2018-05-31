65fa09a0f8f678ef6eddbdb125f2302448fc449b
elmermauricio <39804892+elmermauricio@users.noreply.github.com>
2018-05-31 17:18:01 -0500

    sitio

--- Pruebaunoparcial/Pruebaunoparcial/wwwroot/js/site.js
+++ Pruebaunoparcial/Pruebaunoparcial/wwwroot/js/site.js
@@ -11,3 +11,42 @@ var grabarseguimiento = () => {
     var graba = new claseepleado(nombre, accion);    
     graba.GuardarEmple;
 }
+var grabaEstudio = () => {
+    var estudio = document.getElementById("estudio").value;
+    var estudioId = document.getElementById("estudioId").value;
+    if (estudioId == '') {
+        estudioId = '0';
+        var accion = 'Estudio/ControladorGuardaEstudio';
+    } else {
+        var accion = 'Estudio/ControladorEditaEstudio';
+    }
+    var graba = new ClaseEstudio(estudio, accion);
+    graba.GuardarEstudio(estudioId);
+}
+
+var ListaEstudios = () => {
+    var accion = 'Estudio/ControladorListaEstudio';
+    var estudio = new ClaseEstudio('', accion);
+    estudio.ListadeEstudios();
+}
+
+var CargaEstudio = (estudioId) => {
+    var accion = 'Estudio/ControladorUnEstudio';
+
+    var unestudio = new ClaseEstudio('', accion);
+    unestudio.CargarEstudio(estudioId);
+}
+
+var eliminaEstudio = (id) => {
+    var accion = 'Estudio/ControladorEliminarEstudio';
+    var eliminaestudio = new ClaseEstudio('', accion);
+    var res = confirm('Desea Eliminar el registro');
+
+    if (res == true) {
+        eliminaestudio.EliminarEstudio(id);
+        alert('Reguistro Eliminado');
+
+    } else {
+        alert('Usted cancelo la eliminacion del reguistro');
+    }
+}
